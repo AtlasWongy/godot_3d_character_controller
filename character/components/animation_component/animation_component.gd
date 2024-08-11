70c02d1ca0c183c1ba42d976ade76558265335bf
AtlasWongy <atlasyjyj@gmail.com>
2024-08-11 16:03:09 +0800

    Animation for Crouching and Emoting

--- character/components/animation_component/animation_component.gd
+++ character/components/animation_component/animation_component.gd
@@ -8,6 +8,9 @@ var tween: Tween
 
 func _ready() -> void:
 	player.set_movement_state.connect(_on_set_movement_state)
+	player.set_crouch_state.connect(_on_set_crouch_state)
+	player.set_emoting_state.connect(_on_set_emoting_state)
+	player.set_default_state.connect(_on_set_default_state)
 
 func _on_set_movement_state(_movement_state: MovementState):
 	if tween:
@@ -16,3 +19,32 @@ func _on_set_movement_state(_movement_state: MovementState):
 	tween = create_tween()
 	tween.tween_property(animation_tree, "parameters/movement_blend/blend_position", _movement_state.id, 0.25)
 	tween.parallel().tween_property(animation_tree, "parameters/movement_anim_speed/scale", _movement_state.animation_speed, 0.7)
+
+func _on_set_crouch_state(_crouch_state: bool):
+	
+	_crouch_state = !_crouch_state
+	
+	if _crouch_state:
+		animation_tree["parameters/transition/transition_request"] = "crouch"
+		
+		if tween:
+			tween.kill()
+			
+		tween = create_tween()
+		tween.tween_property(animation_tree, "parameters/crouch_blend/blend_position", 1, 0.8)
+		
+	else:
+		if tween:
+			tween.kill()
+			
+		tween = create_tween()
+		await tween.tween_property(animation_tree, "parameters/crouch_blend/blend_position", 0, 0.8).finished
+		animation_tree["parameters/transition/transition_request"] = "movement"
+		
+func _on_set_emoting_state(_emote_state: bool):
+	animation_tree["parameters/transition/transition_request"] = "movement"
+	animation_tree["parameters/emote_one_shot/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
+	
+func _on_set_default_state():
+	animation_tree["parameters/emote_one_shot/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT
+	animation_tree["parameters/transition/transition_request"] = "movement"
--- character/player/player.gd
+++ character/player/player.gd
@@ -3,13 +3,20 @@ class_name Player
 
 signal set_movement_state(_movement_state: MovementState)
 signal set_movement_direction(_movement_direction: Vector3)
+signal set_crouch_state(_crouch_state: bool)
+signal set_emoting_state(_emote_state: bool)
+signal set_default_state()
 
 @export var movement_states: Dictionary
 
 var movement_direction: Vector3
+var is_crouching: bool = false
+var is_emoting: bool = false
 
 func _input(event):
 	if event.is_action("movement"):
+		set_default_state.emit()
+		
 		movement_direction.x = Input.get_action_strength("left") - Input.get_action_strength("right")
 		movement_direction.z = Input.get_action_strength("forward") - Input.get_action_strength("backwards")
 		
@@ -22,8 +29,13 @@ func _input(event):
 			set_movement_state.emit(movement_states["idle"])
 	
 	if event.is_action_pressed("crouch"):
-		print("I am gonna crouch")
-	
+		set_crouch_state.emit(is_crouching)
+		is_crouching = !is_crouching
+		
+	if event.is_action_pressed("emote"):
+		set_emoting_state.emit(is_emoting)
+		is_emoting = !is_emoting
+		
 func _ready() -> void:
 	set_movement_state.emit(movement_states["idle"])
 	
